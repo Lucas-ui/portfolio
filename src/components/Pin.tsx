b3b30edd7ff966c1e7f28c695e9c26a68c481d67
Lucas-ui <lucas7500@hotmail.fr>
2024-02-07 20:34:20 +0100

    Setup

---
+++ src/components/Pin.tsx
@@ -0,0 +1,49 @@
+function Pin() {
+  return (
+    <>
+      <div className="formations">
+        <p>Formations</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          mollitia cumque suscipit commodi doloremque inventore! Vero, corrupti
+          sit cum perferendis minus aperiam ad reprehenderit sapiente illo
+          inventore totam blanditiis sint quis, officiis accusamus. Dicta et
+          consectetur odit error, consequuntur doloribus iure neque ullam, nihil
+          debitis aliquam! Nostrum alias hic saepe.
+        </p>
+      </div>
+      <div className="projets">
+        <p>Projets</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          mollitia cumque suscipit commodi doloremque inventore! Vero, corrupti
+          sit cum perferendis minus aperiam ad reprehenderit sapiente illo
+          inventore totam blanditiis sint quis, officiis accusamus. Dicta et
+          consectetur odit error, consequuntur doloribus iure neque ullam, nihil
+          debitis aliquam! Nostrum alias hic saepe.
+        </p>
+      </div>
+      <div className="competences">
+        <p>Compétences</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          mollitia cumque suscipit commodi doloremque inventore! Vero, corrupti
+          sit cum perferendis minus aperiam ad reprehenderit sapiente illo
+          inventore totam blanditiis sint quis, officiis accusamus. Dicta et
+          consectetur odit error, consequuntur doloribus iure neque ullam, nihil
+          debitis aliquam! Nostrum alias hic saepe.
+        </p>
+      </div>
+      <div className="contact">
+        <p className="contact-text">Contact</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          mollitia cumque suscipit commodi doloremque inventore! Vero, corrupti
+          sit cum.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Pin;
